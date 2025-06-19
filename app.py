38f6dafc2669af54dcb064741ee9e8650702d15b
kmahaseth1 <136269836+kmahaseth1@users.noreply.github.com>
2025-06-19 00:12:47 -0400

    Layout setup for the dashboard

--- app.py
+++ app.py
@@ -5,3 +5,14 @@ from dash import Dash, dcc, html
 
 # Create a Dash object
 app = Dash(__name__)
+
+# Define the dashboard layout
+app.layout = html.Div(
+    children=[
+        html.H1(children="2020s in Film")
+    ]
+)
+
+# Run the dashboard
+if __name__ == "__main__":
+    app.run(debug=True)
