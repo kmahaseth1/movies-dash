094b9c196a2b62155f9915e85e1c72ec22317cf1
kmahaseth1 <136269836+kmahaseth1@users.noreply.github.com>
2025-06-21 12:22:38 -0400

    Add code to process the data

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 import pandas as pd
 from dash import Dash, dcc, html
 
-# import data from data.py
+# import data
+# Define time frame for the data pull
+begin = 2020
+end = 2025
+
+# Call the get_ids_by_year function to get all the movie ids for 2020s
+tmdb_ids = []
+for year in range(begin, end+1):
+    year_ids = get_ids_by_year(year)
+    tmdb_ids.extend(year_ids)
 
 # Create a Dash object
 app = Dash(__name__)
