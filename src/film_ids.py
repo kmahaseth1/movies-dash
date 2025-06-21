d7395fad841ddd77e75f95fdc475372190feab5a
kmahaseth1 <136269836+kmahaseth1@users.noreply.github.com>
2025-06-21 12:22:07 -0400

    Remove test code since the validation is complete

--- src/film_ids.py
+++ src/film_ids.py
@@ -47,13 +47,4 @@ def get_ids_by_year(year):
         time.sleep(0.1)
     
     return ids
-
-# Define time frame for the data pull
-begin = 2020
-end = 2025
-
-# Call the get_ids_by_year function to get all the movie ids for 2020s
-tmdb_ids = []
-for year in range(begin, end+1):
-    year_ids = get_ids_by_year(year)
-    tmdb_ids.extend(year_ids)
+    
