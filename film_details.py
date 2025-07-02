aed7c53b8f16fc4f2f3ccb0240cd240168a291ec
kmahaseth1 <136269836+kmahaseth1@users.noreply.github.com>
2025-07-02 09:57:46 -0400

    Update get_movie_details to handle genres properly
    - Ensure animation is not extracted as the genres
    - Check if a movie is animation and label its type depending on if that is one of the genres

--- film_details.py
+++ film_details.py
@@ -5,7 +5,7 @@ def get_movie_details(details_url, api_key, ids):
     """
     Get information about movies using the TMDB ids 
     """
-    # List to hold the dictionaries of film details
+    # List to hold the film details
     film_details = []
 
     for id in ids:
@@ -17,16 +17,26 @@ def get_movie_details(details_url, api_key, ids):
         except (requests.exceptions.JSONDecodeError) as e:
             continue
 
-        # Extract relevant data
+        # Extract other metrics
         film_dict = {
             'name': film_raw['title'],
             'budget': film_raw['budget'],
-            'genre': film_raw['genres'][0]['name'] if film_raw['genres'] else None,
+            'type': 'live-action',
+            'genre': None,
+            # 'genre': film_raw['genres'][0]['name'] if film_raw['genres'] else None,
             'production_countries': film_raw['production_countries'][0]['name'] if film_raw['production_countries'] else None,
             'revenue': film_raw['revenue'],
             'vote_average': film_raw['vote_average'],
             'release_year': int(film_raw['release_date'][:4])
         }
+
+        # Get genre and film type
+        for i, film in enumerate(film_raw['genres']):
+            if film['genres'] and film.get("genres", "").lower() == "animation":
+                film_dict['type'] = 'animation'
+                if i + 1 < len(film['genres']):
+                    film_dict['genre'] = film['genres'][i + 1].get("genres")
+                break
 
         # Add the film to the list
         film_details.append(film_dict)
