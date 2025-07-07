897b456c1274181de26be201c42c76081ce432d9
kmahaseth1 <136269836+kmahaseth1@users.noreply.github.com>
2025-07-07 12:11:31 -0400

    Remove the loop from get_movie_details since the db script will handle the iterations

--- film_details.py
+++ film_details.py
@@ -8,38 +8,34 @@ def get_movie_details(details_url, api_key, ids):
     # List to hold the film details
     film_details = []
 
-    for id in ids:
-        # API calls for each movie in the list
-        try:
-            film_url = f"{details_url}{id}"
-            r = requests.get(film_url, params={'api_key': api_key}, timeout=(10, 60))
-            film_raw = r.json()
-        except (requests.exceptions.JSONDecodeError) as e:
+    # API calls for each movie in the list
+    try:
+        film_url = f"{details_url}{id}"
+        r = requests.get(film_url, params={'api_key': api_key}, timeout=(10, 60))
+        film_raw = r.json()
+    except (requests.exceptions.JSONDecodeError) as e:
+        return film_details
+
+    # Extract other metrics
+    film_dict = {
+        'name': film_raw['title'],
+        'budget': film_raw['budget'],
+        'genres': [value['name'] for value in film_raw['genres']],
+        'production_countries': film_raw['production_countries'][0]['name'] if film_raw['production_countries'] else None,
+        'revenue': film_raw['revenue'],
+        'vote_average': film_raw['vote_average'],
+        'release_year': int(film_raw['release_date'][:4])
+    } 
+
+    film_dict['type'] = 'animation' if 'animation' in [x.lower() for x in film_dict['genres']] else 'live action'
+    for genre in film_dict['genres']:
+        if genre.lower() == "animation":
             continue
+        else:
+            film_dict['genre'] = genre.lower()
+            break
 
-        # Extract other metrics
-        film_dict = {
-            'name': film_raw['title'],
-            'budget': film_raw['budget'],
-            'genres': [value['name'] for value in film_raw['genres']],
-            'production_countries': film_raw['production_countries'][0]['name'] if film_raw['production_countries'] else None,
-            'revenue': film_raw['revenue'],
-            'vote_average': film_raw['vote_average'],
-            'release_year': int(film_raw['release_date'][:4])
-        } 
-
-        film_dict['type'] = 'animation' if 'animation' in [x.lower() for x in film_dict['genres']] else 'live action'
-        for genre in film_dict['genres']:
-            if genre.lower() == "animation":
-                continue
-            else:
-                film_dict['genre'] = genre.lower()
-                break
-
-        # Add the film to the list
-        film_details.append(film_dict)
-
-        # Brief sleep to respect rate limits
-        time.sleep(0.1)
+    # Add the film to the list
+    film_details.append(film_dict)
 
     return film_details
