41f287a63f4aa7c89e9bdcc81caa6d128c566dcd
kmahaseth1 <136269836+kmahaseth1@users.noreply.github.com>
2025-07-09 16:03:46 -0400

    Drop the table since the original table had one fewer column

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import os
 from dotenv import load_dotenv
 
 """
-id = 628917
+id = 747358
 load_dotenv()
 api_key = os.getenv("TMDB_API")
 details_url = "https://api.themoviedb.org/3/movie/"
@@ -14,9 +14,9 @@ film_dict = get_movie_details(details_url, api_key, id)
 film = pd.DataFrame(film_dict)
 
 print(film)
-"""
+
 con = sqlite3.connect('movies_2020s.db')
-"""
+
 con.execute(
     '''
     CREATE TABLE IF NOT EXISTS movie_data_raw (
@@ -35,17 +35,25 @@ con.execute(
     '''
 )
 film.to_sql('movie_data_raw', con, if_exists='append', index=False)
-"""
+
 cursor = con.cursor()
 
 #cursor.execute('SELECT production_countries FROM movie_data_raw')
 
-cursor.execute('DELETE FROM movie_data_raw')
-con.commit()
-"""
+#cursor.execute('DELETE FROM movie_data_raw')
+#con.commit()
+
 rows = cursor.fetchall()
 # Print the results
 for row in rows:
     print(row)
+
+con.close()
 """
+
+con = sqlite3.connect('movies_2020s.db')
+cursor = con.cursor()
+
+cursor.execute("DROP TABLE IF EXISTS movie_data_raw")
+con.commit()
 con.close()
