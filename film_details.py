259eb708789114a6a24f7d503c65e4a008f3c20a
kmahaseth1 <136269836+kmahaseth1@users.noreply.github.com>
2025-07-09 16:04:14 -0400

    Update data processing to handle empty values
    - Use .get() instead of [] to handle empty values for title, budget, revenue, vote_average, and release_date
    - Convert genres and production countries return value into JSON formatted strings
    - Handle extraction of one production country to account for empty values for production countries

--- film_details.py
+++ film_details.py
@@ -1,7 +1,8 @@
 import requests
 import time
+import json
 
-def get_movie_details(details_url, api_key, ids):
+def get_movie_details(details_url, api_key, id):
     """
     Get information about movies using the TMDB ids 
     """
@@ -11,29 +12,35 @@ def get_movie_details(details_url, api_key, ids):
     # API calls for each movie in the list
     try:
         film_url = f"{details_url}{id}"
-        r = requests.get(film_url, params={'api_key': api_key}, timeout=(10, 60))
+        r = requests.get(film_url, params={'api_key': api_key}, timeout=(60, 60))
         film_raw = r.json()
     except (requests.exceptions.JSONDecodeError) as e:
         return film_details
 
     # Extract other metrics
     film_dict = {
-        'name': film_raw['title'] if film_raw['title'] else None,
-        'budget': film_raw['budget'],
-        'genres': [value['name'] for value in film_raw['genres']],
-        'production_countries': film_raw['production_countries'][0]['name'] if film_raw['production_countries'] else None,
-        'revenue': film_raw['revenue'],
-        'vote_average': film_raw['vote_average'],
-        'release_year': int(film_raw['release_date'][:4])
+        'name': film_raw.get('title'),
+        'budget': film_raw.get('budget'),
+        'genres': json.dumps([value['name'] for value in film_raw.get('genres', [])]),
+        'production_countries': json.dumps([value['name'] for value in film_raw.get('production_countries', [])]),
+        'revenue': film_raw.get('revenue'),
+        'vote_average': film_raw.get('vote_average'),
+        'release_year': film_raw.get('release_date', '')[:4]
     } 
 
     film_dict['type'] = 'animation' if 'animation' in [x.lower() for x in film_dict['genres']] else 'live action'
-    for genre in film_dict['genres']:
+    genres = json.loads(film_dict['genres'])
+    for genre in genres:
         if genre.lower() == "animation":
             continue
         else:
-            film_dict['genre'] = genre.lower()
+            film_dict['genre'] = genre.title()
             break
+    production_countries = json.loads(film_dict['production_countries'])
+    if production_countries: 
+        film_dict['production_country'] = production_countries[0]
+    else:
+        film_dict['production_country'] = None
 
     # Add the film to the list
     film_details.append(film_dict)
